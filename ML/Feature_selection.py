697cbe2fc425362fa1c9d5e799c79477a4a13d8d
AfrozSheikh <sa8329925@gmail.com>
2024-10-18 18:54:09 +0530

    standardization method added

---
+++ ML/Feature_selection.py
@@ -0,0 +1,24 @@
+# Normalizing the data 
+#standardization 
+import seaborn as sns 
+import numpy as np
+import matplotlib.pyplot as plt
+df = sns.load_dataset('tips')
+total_Bill = list(df["total_bill"])
+mean = np.mean(total_Bill)
+std = np.std(total_Bill)
+# apply z-score formula for every data 
+normalized_daata = [] 
+for i in total_Bill:
+    normalized_daata.append((i - mean) / std)
+
+sns.histplot(normalized_daata)
+
+
+
+# sam can be done by 
+from sklearn.preprocessing import StandardScaler
+scalar = StandardScaler()
+scalar.fit(df[['total_bill']])
+a = scalar.transform(df[['total_bill']])
+
