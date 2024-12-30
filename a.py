965049b3811ede2b6e0d9be38f9ee0054553c03e
AfrozSheikh <sa8329925@gmail.com>
2024-12-30 23:03:50 +0530

    Naive Bayes algo  added

--- a.py
+++ a.py
@@ -1,9 +1,15 @@
-from Babaji import HeightPredictor
+# from Babaji import HeightPredictor
 
-# Initialize the predictor (no need to pass URLs)
-predictor = HeightPredictor()
+# # Initialize the predictor (no need to pass URLs)
+# predictor = HeightPredictor()
 
-# Predict height for a given weight
-  # Example input
-predicted_height = predictor.predict(145)
-print(predicted_height)
+# # Predict height for a given weight
+#   # Example input
+# predicted_height = predictor.predict(145)
+# print(predicted_height)
+
+# from Babaji import DiabeticPredictor
+# a = DiabeticPredictor()
+# c = a.predict([1,2,3,4,4,3,2,1])
+# print(c)
+
